7dcada612ca7c62ca73e740b50bfa086f52c96a2
bridiver <github@brianjohnson.cc>
2021-11-02 16:26:40 -0700

    add httpse component installer

--- chromium_src/chrome/browser/component_updater/registration.cc
+++ chromium_src/chrome/browser/component_updater/registration.cc
@@ -9,8 +9,9 @@
 #include "../../../../../chrome/browser/component_updater/registration.cc"  // NOLINT
 #undef RegisterComponentsForUpdate
 
-#include "chrome/browser/browser_process.h"
+#include "brave/browser/brave_shields/https_everywhere_component_installer.h"
 #include "brave/components/brave_wallet/browser/wallet_data_files_installer.h"
+#include "chrome/browser/browser_process.h"
 
 namespace component_updater {
 
@@ -21,6 +22,7 @@ void RegisterComponentsForUpdate(bool is_off_the_record_profile,
                                            profile_prefs, profile_path);
   ComponentUpdateService* cus = g_browser_process->component_updater();
   brave_wallet::RegisterWalletDataFilesComponent(cus);
+  brave_shields::RegisterHTTPSEverywhereComponent(cus);
 }
 
 }  // namespace component_updater
